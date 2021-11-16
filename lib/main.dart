71aac5cf5927eeba22ef7b281a6f2107ce8ad989
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-11-16 21:30:09 +0700

    updaet main dart: using material router

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_fam_web1/app_route_path.dart';
 import 'package:flutter_fam_web1/mainpage.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
@@ -15,9 +17,76 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const MainPage(),
+    return MaterialApp.router(
+      routeInformationParser: AppRouteInformationParser(),
+      routerDelegate: AppRouterDelegate(),
+      scrollBehavior: MyCustomScrollBehavior(),
       debugShowCheckedModeBanner: false,
+      title: 'Fam Web Training',
+      // home: const MainPage(),
     );
+  }
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
+}
+
+class AppRouteInformationParser extends RouteInformationParser<AppRoutePath> {
+  @override
+  Future<AppRoutePath> parseRouteInformation(
+      RouteInformation routeInformation) {
+    // TODO: implement parseRouteInformation
+    throw UnimplementedError();
+  }
+}
+
+class AppRouterDelegate extends RouterDelegate<AppRoutePath>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin<AppRoutePath> {
+  @override
+  GlobalKey<NavigatorState>? get navigatorKey => GlobalKey<NavigatorState>();
+
+  @override
+  void addListener(VoidCallback listener) {
+    // TODO: implement addListener
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      pages: const [
+        MaterialPage(
+          name: '/',
+          child: MainPage(),
+        ),
+      ],
+      onPopPage: (route, result) {
+        if (!route.didPop(result)) return false;
+        print("route of app: $route");
+        return true;
+      },
+    );
+  }
+
+  @override
+  Future<bool> popRoute() {
+    // TODO: implement popRoute
+    throw UnimplementedError();
+  }
+
+  @override
+  void removeListener(VoidCallback listener) {
+    // TODO: implement removeListener
+  }
+
+  @override
+  Future<void> setNewRoutePath(configuration) {
+    // TODO: implement setNewRoutePath
+    throw UnimplementedError();
   }
 }
