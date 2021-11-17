c5e7cc400250285cc892ea38c71f5a61621ee88e
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-11-17 12:22:42 +0700

    update main dart: first try

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fam_web1/app_route_path.dart';
 import 'package:flutter_fam_web1/mainpage.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,13 +18,17 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routeInformationParser: AppRouteInformationParser(),
-      routerDelegate: AppRouterDelegate(),
-      scrollBehavior: MyCustomScrollBehavior(),
-      debugShowCheckedModeBanner: false,
-      title: 'Fam Web Training',
-      // home: const MainPage(),
+    return ScreenUtilInit(
+      builder: () {
+        return MaterialApp.router(
+          title: 'Fam Web Training',
+          debugShowCheckedModeBanner: false,
+          scrollBehavior: MyCustomScrollBehavior(),
+          routeInformationParser: AppRouteInformationParser(),
+          routerDelegate: AppRouterDelegate(),
+          // home: const MainPage(),
+        );
+      }
     );
   }
 }
@@ -39,9 +44,53 @@ class MyCustomScrollBehavior extends MaterialScrollBehavior {
 class AppRouteInformationParser extends RouteInformationParser<AppRoutePath> {
   @override
   Future<AppRoutePath> parseRouteInformation(
-      RouteInformation routeInformation) {
-    // TODO: implement parseRouteInformation
-    throw UnimplementedError();
+      RouteInformation routeInformation) async {
+    final uri = Uri.parse(routeInformation.location.toString());
+    // ignore: avoid_print
+    print("uri: $uri");
+
+    // handle /
+    if (uri.pathSegments.isEmpty) {
+      return AppRoutePath.home();
+    }
+
+    // handle /home
+    else if (uri.pathSegments.length == 1) {
+      final first = uri.pathSegments[0].toLowerCase();
+      if (first == 'home') {
+        return AppRoutePath.home();
+      }
+      if (first == '404') {
+        return AppRoutePath.unknown();
+      }
+    }
+
+    // // handle 'project/:id'
+    // else if (uri.pathSegments.length == 2) {
+    //   if (uri.pathSegments.first == 'project') {
+    //     // if (uri.pathSegments.first == 'project/:id') {
+    //     final remaining = uri.pathSegments.elementAt(1);
+    //     final id = int.tryParse(remaining);
+    //     if (id == null) {
+    //       return AppRoutePath.unknown();
+    //     }
+    //     return AppRoutePath.projectDetails(id);
+    //   }
+    // }
+
+    else {
+      // handle 'unknown page'
+      return AppRoutePath.unknown();
+    }
+    return AppRoutePath.unknown();
+  }
+
+  @override
+  RouteInformation? restoreRouteInformation(AppRoutePath configuration) {
+    if (configuration.isUnknown)
+      return const RouteInformation(location: '/404');
+    if (configuration.isHome) return const RouteInformation(location: '/home');
+    return null;
   }
 }
 
@@ -51,9 +100,7 @@ class AppRouterDelegate extends RouterDelegate<AppRoutePath>
   GlobalKey<NavigatorState>? get navigatorKey => GlobalKey<NavigatorState>();
 
   @override
-  void addListener(VoidCallback listener) {
-    // TODO: implement addListener
-  }
+  void addListener(VoidCallback listener) {}
 
   @override
   Widget build(BuildContext context) {
@@ -61,12 +108,14 @@ class AppRouterDelegate extends RouterDelegate<AppRoutePath>
       key: navigatorKey,
       pages: const [
         MaterialPage(
-          name: '/',
+          key: ValueKey('home'),
+          name: '/home',
           child: MainPage(),
         ),
       ],
       onPopPage: (route, result) {
         if (!route.didPop(result)) return false;
+        // ignore: avoid_print
         print("route of app: $route");
         return true;
       },
@@ -74,19 +123,16 @@ class AppRouterDelegate extends RouterDelegate<AppRoutePath>
   }
 
   @override
-  Future<bool> popRoute() {
-    // TODO: implement popRoute
-    throw UnimplementedError();
+  Future<bool> popRoute() async {
+    return true;
+    // throw UnimplementedError();
   }
 
   @override
-  void removeListener(VoidCallback listener) {
-    // TODO: implement removeListener
-  }
+  void removeListener(VoidCallback listener) {}
 
   @override
-  Future<void> setNewRoutePath(configuration) {
-    // TODO: implement setNewRoutePath
-    throw UnimplementedError();
+  Future<void> setNewRoutePath(AppRoutePath configuration) async {
+    // throw UnimplementedError();
   }
 }
