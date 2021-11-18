f041633ca34a3a1301253347868b31070205cf25
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-11-18 12:47:13 +0700

    update mainpage

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fam_web1/data/listnames_data.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -18,6 +20,18 @@ class _MainPageState extends State<MainPage> {
           style: GoogleFonts.poppins(
             fontSize: 20,
           ),
+        ),
+      ),
+      body: Consumer(
+        builder: (context, ListNameData data, _) => ListView.builder(
+          shrinkWrap: true,
+          itemCount: data.data.length,
+          itemBuilder: (context, index){
+            return ListTile(
+              title: Text("${data.data[index].name}"),
+              subtitle: Text("${data.data[index].desc}"),
+            );
+          },
         ),
       ),
     );
