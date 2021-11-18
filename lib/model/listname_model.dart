c75ba46f1164c523f7dd2e1ed9f1b81db5df1963
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-11-18 12:46:43 +0700

    add listname model

---
+++ lib/model/listname_model.dart
@@ -0,0 +1,13 @@
+class ListNameModel {
+  final String? id;
+  final String? name;
+  final String? desc;
+  final DateTime? birthDate;
+
+  ListNameModel({
+    this.id,
+    this.name,
+    this.desc,
+    this.birthDate,
+  });
+}
