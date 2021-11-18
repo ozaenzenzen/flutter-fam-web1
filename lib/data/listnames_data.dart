98f92cf5107ca66fe5fbb2c738f77e27d30c2467
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-11-18 12:46:54 +0700

    add listname data

---
+++ lib/data/listnames_data.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_fam_web1/model/listname_model.dart';
+import 'package:mock_data/mock_data.dart';
+
+class ListNameData extends ChangeNotifier{
+  ListNameData._(){
+    getData();
+  }
+
+  static final ListNameData listNameData = ListNameData._();
+
+  List data = [];
+
+  getData(){
+    for(var i  =0; i<20;i++){
+      data.add(
+        ListNameModel(
+          id: i.toString(),
+          name: mockName('male'),
+          desc: mockString(6,'a#'),
+          birthDate: mockDate(DateTime(1995), DateTime(2002))
+        ),
+      );
+    }
+  }
+}
